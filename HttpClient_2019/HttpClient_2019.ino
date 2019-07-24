d77f73f3168a962a8df37dd87527f26e20ff851c
Travis Golob <feralmonkey@users.noreply.github.com>
2019-07-24 09:07:27 -0700

    Update HttpClient_2019.ino
    Making prod code better!

--- HttpClient_2019/HttpClient_2019.ino
+++ HttpClient_2019/HttpClient_2019.ino
@@ -8,16 +8,16 @@
 
 ESP8266WiFiMulti WiFiMulti;
 
-void setup() {
+// global variables
+String services[] = { "skypeforbusiness", "teams", "exchange", "sharepoint", "onedrive" };
+int services_length = 5;
+String api_uri = "http://audiapifunction.azurewebsites.net/api/audiapi?code=cvp43XxTZwWkLSEZLUazqmboF8BsAPoVwIBYmlAtEf/lffYYHbQ1bQ==";
 
+void setup() {
   Serial.begin(115200);
   // Serial.setDebugOutput(true);
 
-  Serial.println();
-  Serial.println();
-  Serial.println();
-
-  Serial.println("[SETUP] HOLD UP; WAIT A MINUTE...\n");
+  Serial.println("HOLD UP; WAIT A MINUTE...\n");
   for (uint8_t t = 4; t > 0; t--) {
     Serial.flush();
     delay(1000);
@@ -25,12 +25,10 @@ void setup() {
 
   WiFi.mode(WIFI_STA);
   WiFiMulti.addAP("HACKWIFI", "redmond19");
-
 }
 
 void loop() {
-  String services[] = { "skypeforbusiness", "teams", "exchange", "sharepoint", "onedrive" };
-  
+
   // wait for WiFi connection
   if ((WiFiMulti.run() == WL_CONNECTED)) { 
     WiFiClient client;
@@ -38,7 +36,7 @@ void loop() {
     HTTPClient http;
 
     Serial.print("[HTTP] begin...\n");
-    if (http.begin(client, "http://audiapifunction.azurewebsites.net/api/audiapi?code=cvp43XxTZwWkLSEZLUazqmboF8BsAPoVwIBYmlAtEf/lffYYHbQ1bQ==")) {  // HTTP
+    if (http.begin(client, api_uri.c_str())) {  // HTTP
 
       Serial.print("[HTTP] GET...\n");
       // start connection and send HTTP header
@@ -67,7 +65,7 @@ void loop() {
 
           String service_to_measure;
           bool failure_bool;
-          for (int n = 0; n < 5; n++){
+          for (int n = 0; n < services_length; n++){
               service_to_measure = services[n];
 
               if(!root.success()) {
@@ -93,7 +91,7 @@ void loop() {
  
       http.end();
     } else {
-      Serial.printf("[HTTP} Unable to connect because Stewart's face is weird... \n");
+      Serial.printf("[HTTP} Unable to connect\n");
     }
   }
   Serial.println("\n");
